58601a50c14edf512c618388717b099ae7652749
Sazzad ur Rahman <22joylobo@gmail.com>
2022-03-01 22:22:29 +0600

    almost

--- js/main.js
+++ js/main.js
@@ -72,8 +72,60 @@ const loadDetails = phoneId => {
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
     fetch(url)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(datas => displayDetails(datas.data))
     
 
 }
 
+// for displaying details
+const displayDetails = phone => {
+
+    console.log(phone);
+
+    const detailsDiv = document.getElementById("details-div");
+
+    const div = document.createElement("div");
+
+    // clearing previous details
+    detailsDiv.textContent = ''; 
+
+    div.classList.add('row');
+    div.classList.add('g-0')
+
+    // release date
+    /* const releaseDate;
+    if (phone.hasOwnProperty('releaseDate')) {
+         releaseDate = phone.releaseDate;
+    }
+    else {
+         releaseDate = 'No Release date available';
+    }
+    console.log(releaseDate); */
+
+    div.innerHTML = `
+    <div class="col-md-4">
+          <img src="..." class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">${phone.name}</h5>
+            <p class="card-text">
+              <span class="text-decoration-underline">Display</span> : is a wider card
+              <br>
+              <span class="text-decoration-underline">Chipset</span> : is a wider card
+              <br>
+              <span class="text-decoration-underline">Storage</span> : is a wider card
+              <br>
+              <span class="text-decoration-underline">Memory</span> : is a wider card
+              <br>
+              <span class="text-decoration-underline">Release Date</span> : is a wider card
+              <br>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+             </p>
+            
+          </div>
+        </div>
+    ` ;
+    detailsDiv.appendChild(div);
+
+}
