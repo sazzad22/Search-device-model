b9825521878cd4f36d6a09a72c56172588f678b2
Sazzad ur Rahman <22joylobo@gmail.com>
2022-03-01 19:05:35 +0600

    displaying results

--- js/main.js
+++ js/main.js
@@ -7,7 +7,8 @@ const searchText = () => {
     // clearing search field input
     searchField.value = '';
 
-    fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchFieldText}`
+    fetch(url)
         .then(response => response.json())
         .then(datas => displayResults(datas.data))
     
@@ -19,10 +20,34 @@ const searchText = () => {
 // for displaying results
 const displayResults = (phones) => {
     
-    console.log(phones);
+    // console.log(phones);
     const resultDiv = document.getElementById('result-div');
 
+    //clearing previous results
+    resultDiv.textContent = '';
+
     
+
+    phones.forEach((phone) => {
+        console.log(phone.phone_name);
+        const div = document.createElement('div');
+
+        div.classList.add('col');
+
+        div.innerHTML = `
+        <div class="col">
+          <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${phone.phone_name}</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        </div>
+        `
+        resultDiv.appendChild(div);
+
+    })
 
     
 
